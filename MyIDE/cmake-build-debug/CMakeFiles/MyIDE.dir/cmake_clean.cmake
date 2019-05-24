d4d8b082fc77bb2c3f0c21923b9e34bfba2ed758
Bojorge <38023924+Bojorge@users.noreply.github.com>
2019-05-24 12:06:16 -0600

    MyIDE

---
+++ MyIDE/cmake-build-debug/CMakeFiles/MyIDE.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyIDE.dir/main.cpp.o"
+  "MyIDE.pdb"
+  "MyIDE"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyIDE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

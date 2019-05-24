8fd10393ab522932aef72fd16b6f5bf0f87759f7
Bojorge <38023924+Bojorge@users.noreply.github.com>
2019-05-24 11:15:23 -0600

    ServerLibrary

---
+++ ServerLibrary/cmake-build-debug/CMakeFiles/ServerLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ServerLibrary.dir/main.cpp.o"
+  "ServerLibrary.pdb"
+  "ServerLibrary"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ServerLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

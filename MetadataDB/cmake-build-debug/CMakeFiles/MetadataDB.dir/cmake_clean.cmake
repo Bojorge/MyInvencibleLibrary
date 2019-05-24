4aa4f9c174a811862ec16439f988d2c0094b5b6e
Bojorge <38023924+Bojorge@users.noreply.github.com>
2019-05-24 12:18:11 -0600

    MetadataDB

---
+++ MetadataDB/cmake-build-debug/CMakeFiles/MetadataDB.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MetadataDB.dir/main.cpp.o"
+  "MetadataDB.pdb"
+  "MetadataDB"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MetadataDB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
